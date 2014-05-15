d3e187bfcf6e3d28046da8e1e5842876982760ad
Adam Price <aesprice@gmail.com>
2014-05-15 14:07:04 -0700

    Handles non-array objects (also added fixes for variable scope and ignoring functions and undefined values)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,11 +8,13 @@ var stringifyJSON = function(obj) {
 		return String(obj);
 	}else if(typeof(obj) === "string"){
 		return '"' + obj + '"'
+	}else if(typeof(obj) === "function" || typeof(obj) === "undefined"){ // Don't bother with functions and undefined values
+		return
 	}else if(typeof(obj) != "object"){
 		return obj.toString()
 	}
 
-	objString = ""; // Create a meta-string variable so we can build more complex stringifications
+	var objString = ""; // Create a meta-string variable so we can build more complex stringifications
 
 	if(Array.isArray(obj)){
 		objString += "[";
@@ -20,11 +22,25 @@ var stringifyJSON = function(obj) {
 		_.each(obj, function(item, index){
 			objString += stringifyJSON(item);
 			// Because we're calling this function, if the item is an object then it will return a string with all of the item's items
-			if(index != obj.length - 1){
+			if(index < obj.length - 1){
 				objString += ",";
 			}
 		});
 		objString += "]";
+	}else{
+		objString += "{";
+		var itemCount = 0;
+		// Same as the array, but this time with an object wrapper and our own means of item indexing
+		_.each(obj, function(item, key){
+			if(typeof(item) != "function" && typeof(item) != "undefined"){ // Again, don't bother with functions and undefined values, even in objects
+				objString += '"' + key + '"' + ":" + stringifyJSON(item);
+				itemCount += 1;
+				if(itemCount < _.size(obj)){
+					objString += ",";
+				}
+			}
+		});
+		objString += "}";
 	}
 
 	return objString;
