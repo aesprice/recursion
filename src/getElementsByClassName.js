6f54c0f60dfc80f3ca161d62f160daa8df900516
Adam Price <aesprice@gmail.com>
2014-05-15 19:00:52 -0700

    Starting getElementsByClassName, includes basic recursive functionality

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,34 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+	elementList = []; // Array to hold all matching elements
+
+	var body = document.body;
+	// Check body for a class first
+	if(body.className === className){
+		elementList.push(body);
+	}
+
+	// Define function to check childen recursively
+	findChildren = function(node){
+		_.each(node.childNodes, function(item){
+			// Add matching children to the list
+			if(item.className === className){
+				elementList.push(item);
+			}
+
+			// Recursively check their children
+			if(typeof(item) === "object"){
+				findChildren(item);
+			}
+		});
+	}
+
+	console.log("About to test");
+	// Use body as a starting point to look through children
+	findChildren(body);
+
+	console.log(elementList);
+	console.log("Tested");
+	return elementList;
 };
